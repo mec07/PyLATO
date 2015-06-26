eef7a82cd06609e9b464adecfe488f1288b6be3a
mb4512 <mb4512@ic.ac.uk>
2015-06-26 13:13:33 +0100

    Merged TBgeom.py with TBinit.py

--- TBinit.py
+++ TBinit.py
@@ -8,11 +8,11 @@ This module carries out the needed initialisation tasks
 #
 # Import modules
 import os, sys, importlib
-import TBgeom
 import TBH
 import TBelec
+import TBIO
 import commentjson
-
+from Verbosity import *
 
 class InitJob:
     """Set up the job, build the initial geometry, hamiltonian, and electronic structure."""
@@ -23,11 +23,6 @@ class InitJob:
         with open(jobfile, 'r') as inputfile:
             self.Def = commentjson.loads(inputfile.read())
 
-        #with open(atomicfile, 'r') as afile:
-        #    self.Atomic = commentjson.loads(afile.read())
-
-        # Model Import
-        #
         # Fetch the model name path from the Job file
         modelname = self.Def['model']
         modelpath = os.path.join("models", modelname + ".py")
@@ -44,10 +39,29 @@ class InitJob:
         self.Model = model_module.MatrixElements(os.path.join("models", modelname + ".json"))
 
         # Initialise the geometry
-        TBgeom.init(self)
-        #
+        self.init_geom(self.Def['gy_file'])
+
         # Initialise the Hamiltonian class
         self.Hamilton = TBH.Hamiltonian(self)
-        #
+
         # Initialise the electron module
         self.Electron = TBelec.Electronic(self)
+
+    def init_geom(self, filepath):
+		"""initialise the geometry."""
+
+		# Read in the geometry from file
+		NAtom, Pos, AtomType = TBIO.ReadGeom(filepath)
+
+		# Write out the geometry
+		TBIO.WriteXYZ(self, NAtom, '', AtomType, Pos)
+
+		# Transfer geometry to the JobClass
+		self.NAtom    = NAtom
+		self.Pos      = Pos
+
+		self.AtomType = AtomType
+		self.NOrb     = [self.Model.atomic[self.AtomType[a]]['NOrbitals'] for a in range(self.NAtom)]
+
+		verboseprint(self.Def['verbose'], "Atom positions:")
+		verboseprint(self.Def['verbose'], self.Pos)
