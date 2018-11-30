604dd821e8b347237603dec940b9453aa1eb1e4f
Marc Coury <gloriphobia@gmail.com>
2018-11-30 10:25:40 +0000

    added missing files

---
+++ pylato/pauli_spin_matrices.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+sigma_x = np.array([
+    [0, 1],
+    [1, 0]
+])
+
+sigma_y = np.array([
+    [0, complex(0, -1)],
+    [complex(0, 1), 0]
+])
+
+sigma_z = np.array([
+    [1, 0],
+    [0, -1]
+])
